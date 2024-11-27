c68253838eb6939e9037cfb1e08ed90949983098
Rakesh <rakesh.mondal29@gmail.com>
2024-11-27 19:30:57 +0530

    python file updated

--- python.py
+++ python.py
@@ -1,4 +1,20 @@
-import pandas as pd
-df = pd.DataFrame({'Name':['Rakesh','Rishab'],'Club':['SRH','DC']})
-print(df)
+
+a = [[2,3,5],[1,2,4,6]]
+new_list1 = []
+for i in a:
+    for j in i:
+        new_list1.append(j)
+print(new_list1)
+
+a = [[2,3,5],[1,2,4,6],[2,5,7,[12,13]]]
+new_list = []
+for i in a:
+    for j in i:
+        new_list.append(j)
+        if type(j)==list:
+            new_list.remove(j)
+            for k in j:
+                new_list.append(k)
+                
+print(new_list)
                    
